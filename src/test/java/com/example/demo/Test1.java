486dd480da492e4d1be18d454ee7771fd101f9c9
mengxiangrong <1079850137@qq.com>
2020-12-09 17:20:14 +0800

    Revert "增加test55"
    This reverts commit 3321ec34

--- src/test/java/com/example/demo/Test1.java
+++ src/test/java/com/example/demo/Test1.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class Test1 {
     public void test() {
         System.out.println("11111");
+        System.out.println("2222");
         System.out.println("3333");
         System.out.println("4444");
     }
