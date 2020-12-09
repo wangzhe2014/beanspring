3321ec34899b350b498f18c7b6dfe5119f641490
mengxiangrong <1079850137@qq.com>
2020-12-09 17:20:14 +0800

    增加test55

--- src/test/java/com/example/demo/Test1.java
+++ src/test/java/com/example/demo/Test1.java
@@ -3,7 +3,6 @@ package com.example.demo;
 public class Test1 {
     public void test() {
         System.out.println("11111");
-        System.out.println("2222");
         System.out.println("3333");
         System.out.println("4444");
     }
