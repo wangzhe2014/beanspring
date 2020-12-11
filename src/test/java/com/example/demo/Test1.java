cc82c7fd32274022ad60ea9e2672d912d38aa14c
mengxiangrong <1079850137@qq.com>
2020-12-11 10:02:08 +0800

    Revert "增加test91"
    This reverts commit 5ed45ea8

--- src/test/java/com/example/demo/Test1.java
+++ src/test/java/com/example/demo/Test1.java
@@ -7,6 +7,6 @@ public class Test1 {
         System.out.println("3333");
         System.out.println("4444");
         System.out.println("666");
-        System.out.println("8881");
+        System.out.println("888");
     }
 }
