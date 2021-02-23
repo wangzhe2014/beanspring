27834149f1421dc0c2ec6742c1f60fdfdbc1d608
mengxiangrong <1079850137@qq.com>
2021-02-23 20:29:15 +0800

    1

--- src/test/java/com/example/demo/Test1.java
+++ src/test/java/com/example/demo/Test1.java
@@ -9,7 +9,7 @@ public class Test1 {
         System.out.println("666");
         System.out.println("8881");
         System.out.println("12");
-        System.out.println("13");
+        System.out.println("132");
 
     }
 
