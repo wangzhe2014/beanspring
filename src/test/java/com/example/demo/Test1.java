2d912df8f5211f244d26b065112b7882c24b9f9a
mengxiangrong <1079850137@qq.com>
2021-02-23 21:25:10 +0800

    4

--- src/test/java/com/example/demo/Test1.java
+++ src/test/java/com/example/demo/Test1.java
@@ -8,7 +8,7 @@ public class Test1 {
         System.out.println("4444");
         System.out.println("666");
         System.out.println("8881");
-        System.out.println("12");
+        System.out.println("124");
         System.out.println("1312");
     }
 }
