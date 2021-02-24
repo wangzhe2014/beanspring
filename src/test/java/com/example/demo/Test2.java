500730cb7b28862a23a68ae31f7f04e1aecbc50b
mengxiangrong <1079850137@qq.com>
2021-02-24 10:28:10 +0800

    21and22
    21
    
    22

--- src/test/java/com/example/demo/Test2.java
+++ src/test/java/com/example/demo/Test2.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println(23);
+        System.out.println(235);
     }
 }
